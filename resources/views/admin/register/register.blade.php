77730bfc578d967c602b1a64b889e6c75f3bb3db
mdsayedarif84 <mdsayedarif84@gmail.com>
2021-09-23 11:27:44 +0600

    registraton unique emai and pwd and cpwd matching by javascript

--- resources/views/admin/register/register.blade.php
+++ resources/views/admin/register/register.blade.php
@@ -1,118 +1,139 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta name="description" content="">
+            <meta name="author" content="">
+            <title> Admin- Register</title>
+            <!-- Custom fonts for this template-->
+            <link href="{{ asset('/admin') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+            <link
+                href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+                rel="stylesheet">
+            <!-- Custom styles for this template-->
+            <link href="{{ asset('/admin') }}/css/sb-admin-2.min.css" rel="stylesheet">
 
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>SB Admin 2 - Register</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('/admin') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset('/admin') }}/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
-
-<div class="container">
-
-    <div class="card o-hidden border-0 shadow-lg my-5">
-        <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-                <div class="col-lg-3 d-none d-lg-block bg-success"></div>
-                <div class="col-lg-6">
-                    <div class="p-5">
-                        <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+        </head>
+        <body class="bg-gradient-primary">
+            <div class="container">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-3 d-none d-lg-block bg-success"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                                    </div>
+{{--                                    <form class="user" method="POST" action="{{ route('register') }}">--}}
+                                        {!! Form::open(['class'=>'user','method'=>'POST', 'route'=>'register']) !!}
+{{--                                        @csrf--}}
+                                        <div class="form-group row">
+                                            <div class="col-sm-12">
+                                                <input type="text" name="name" id="exampleLastName" placeholder="Name"
+                                                       class="form-control form-control-user @error('name') is-invalid @enderror"
+                                                       value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                                @error('name')
+                                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control form-control-user @error('email') is-invalid @enderror"
+                                                   id="exampleInputEmail" placeholder="Email Address"
+                                                   value="{{ old('email') }}" required autocomplete="email">
+                                            @error('email')
+                                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                                            @enderror
+                                            <span class="text-danger" id="res"></span>
+                                        </div>
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <input type="password" class="form-control form-control-user  @error('password') is-invalid @enderror"
+                                                       id="exampleInputPassword" name="password"
+                                                       required autocomplete="new-password" placeholder="Password">
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                                                @enderror
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <input type="password" class="form-control form-control-user"
+                                                       name="password_confirmation" required autocomplete="new-password"
+                                                       id="exampleRepeatPassword" placeholder="Confirm Password">
+                                            </div>
+                                            <span class="text-center" id="match"></span>
+                                        </div>
+                                        <button type="submit" class="btn btn-outline-primary btn-user btn-block">
+                                            {{ __('Register Account') }}
+                                        </button>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i>{{ __('Register with Facebook') }}
+                                        </a>
+{{--                                    </form>--}}
+                                    {!! Form::close() !!}
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class=" btn btn-success btn-user btn-block" href="forgot-password.html">{{ __('Forgot Password?') }}</a>
+                                    </div>
+                                    <hr>
+                                        <a class=" btn btn-danger btn-user btn-block " href="{{ route('login') }}">
+                                            <i class="fas fa-sign-in-alt"></i>{{ __('Already have an account? Login!') }}
+                                        </a>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 d-none d-lg-block bg-success"></div>
                         </div>
-                        <form class="user" method="POST" action="{{ route('register') }}">
-                            @csrf
-                            <div class="form-group row">
-                                <div class="col-sm-12">
-                                    <input type="text" name="name" id="exampleLastName" placeholder="Name"
-                                           class="form-control form-control-user @error('name') is-invalid @enderror"
-                                           value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" class="form-control form-control-user @error('email') is-invalid @enderror"
-                                       id="exampleInputEmail" placeholder="Email Address"
-                                       value="{{ old('email') }}" required autocomplete="email">
-                                @error('email')
-                                <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                            <div class="form-group row">
-                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                    <input type="password" class="form-control form-control-user  @error('password') is-invalid @enderror"
-                                           id="exampleInputPassword" name="password"
-                                           required autocomplete="new-password" placeholder="Password">
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                                <div class="col-sm-6">
-                                    <input type="password" class="form-control form-control-user"
-                                           name="password_confirmation" required autocomplete="new-password"
-                                           id="exampleRepeatPassword" placeholder="Confirm Password">
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-outline-primary btn-user btn-block">
-                                {{ __('Register Account') }}
-                            </button>
-                            <hr>
-                            <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                <i class="fab fa-facebook-f fa-fw"></i>{{ __('Register with Facebook') }}
-                            </a>
-                        </form>
-                        <hr>
-                        <div class="text-center">
-                            <a class=" btn btn-success btn-user" href="forgot-password.html">{{ __('Forgot Password?') }}</a>
-                        </div>
-                        <hr>
-                            <a class=" btn btn-danger btn-block " href="{{ route('login') }}">
-                                <i class="fas fa-sign-in-alt"></i>{{ __('Already have an account? Login!') }}
-                            </a>
                     </div>
                 </div>
-                <div class="col-lg-3 d-none d-lg-block bg-success"></div>
             </div>
-        </div>
-    </div>
+            <!-- Bootstrap core JavaScript-->
+            <script src="{{ asset('/admin') }}/vendor/jquery/jquery.min.js"></script>
+            <script src="{{ asset('/admin') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-</div>
+            <!-- Core plugin JavaScript-->
+            <script src="{{ asset('/admin') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
 
-<!-- Bootstrap core JavaScript-->
-<script src="{{ asset('/admin') }}/vendor/jquery/jquery.min.js"></script>
-<script src="{{ asset('/admin') }}/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- Core plugin JavaScript-->
-<script src="{{ asset('/admin') }}/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="{{ asset('/admin') }}/js/sb-admin-2.min.js"></script>
-
-
-</body>
-
-</html>
+            <!-- Custom scripts for all pages-->
+            <script src="{{ asset('/admin') }}/js/sb-admin-2.min.js"></script>
+            <script>
+                $(document).ready(function (){
+                    $('#exampleRepeatPassword').keyup(function (){
+                        var pwd        =   $('#exampleInputPassword').val();
+                        var cpwd        =   $('#exampleRepeatPassword').val();
+                        if (pwd != cpwd){
+                            $('#match').html('Password is not match');
+                            $('#match').css('color','red');
+                            return false;
+                        } else if (pwd == cpwd){
+                            $('#match').html('Password match');
+                            $('#match').css('color','green');
+                            return true;
+                        }
+                    });
+                });
+            </script>
+            <script>
+                var userEmail       =   document.getElementById('exampleInputEmail');
+                userEmail.onblur    =   function (){
+                    var userEmail   =   document.getElementById('exampleInputEmail').value;
+                    var xmlHttp     =   new XMLHttpRequest();
+                    var serverPage  =   "http://localhost/ecommerce/public/register/ajax-user-email-check/"+userEmail;
+                    xmlHttp.open('GET', serverPage);
+                    xmlHttp.onreadystatechange  =   function (){
+                        if( xmlHttp.readyState == 4 && xmlHttp.status  ==  200) {
+                            document.getElementById('res').innerHTML   =   xmlHttp.responseText;
+                            if (xmlHttp.responseText == 'This Email Already exist.Try new email');
+                            document.getElementById('regBtn').disabled  =    true;
+                        } else {
+                            document.getElementById('regBtn').disabled  =    false;
+                        }
+                    }
+                    xmlHttp.send(null);
+                }
+            </script>
+        </body>
+    </html>
